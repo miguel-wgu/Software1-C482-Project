4e26fdff24d3201a1104c7eaeb3b31c5891b222e
Miguel Guzman <53159738+mag-projects@users.noreply.github.com>
2022-10-31 15:19:03 -0700

    Updated Outsourced Javadocs

--- src/model/Outsourced.java
+++ src/model/Outsourced.java
@@ -1,24 +1,45 @@
 package model;
 
-public class Outsourced extends Part{
+/**
+ * The type Outsourced.
+ */
+public class Outsourced extends Part {
+	/**
+	 * The Company name.
+	 */
 	private String companyName;
 
+	/**
+	 * Instantiates a new Outsourced.
+	 *
+	 * @param id          the id.
+	 * @param name        the name.
+	 * @param price       the price.
+	 * @param stock       the stock.
+	 * @param min         the min.
+	 * @param max         the max.
+	 * @param companyName the company name.
+	 */
 	public Outsourced(int id, String name, double price, int stock, int min, int max, String companyName) {
 		super(id, name, price, stock, min, max);
 		this.companyName = companyName;
 	}
 
 	/**
-	 * @param companyName sets the companyName.
-	 */
-	public void setCompanyName(String companyName) {
-		this.companyName = companyName;
-	}
-
-	/**
+	 * Gets company name.
+	 *
 	 * @return returns the companyName.
 	 */
 	public String getCompanyName() {
 		return companyName;
+	}
+
+	/**
+	 * Sets company name.
+	 *
+	 * @param companyName sets the companyName.
+	 */
+	public void setCompanyName(String companyName) {
+		this.companyName = companyName;
 	}
 }
