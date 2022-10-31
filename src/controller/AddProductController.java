c8c511e18f2d5c1be239da0756fcd1cd92416fdf
Miguel Guzman <53159738+mag-projects@users.noreply.github.com>
2022-10-31 14:07:52 -0700

    Cleaned up AddProductController and refactored.

--- src/controller/AddProductController.java
+++ src/controller/AddProductController.java
@@ -29,41 +29,99 @@ import static helper.ErrMsg.isValid;
  * @author Miguel Guzman
  */
 public class AddProductController implements Initializable {
-
+	/**
+	 * The Associated Parts list used to store parts associated with a product.
+	 */
 	ObservableList<Part> associatedParts = FXCollections.observableArrayList();
+	/**
+	 * The Parts table view.
+	 */
 	@FXML
 	private TableView<Part> partsTableView;
+	/**
+	 * The Part id col.
+	 */
 	@FXML
 	private TableColumn<Part, Integer> partIDCol;
+	/**
+	 * The Part name col.
+	 */
 	@FXML
 	private TableColumn<Part, String> partNameCol;
+	/**
+	 * The Part inv lvl col.
+	 */
 	@FXML
 	private TableColumn<Part, Integer> partInvLvlCol;
+	/**
+	 * The Part cost col.
+	 */
 	@FXML
 	private TableColumn<Part, Double> partCostCol;
+	/**
+	 * The Associated parts table view.
+	 */
 	@FXML
 	private TableView<Part> associatedPartsTableView;
+	/**
+	 * The Associated part id col.
+	 */
 	@FXML
 	private TableColumn<Part, Integer> associatedPartIdCol;
+	/**
+	 * The Associated part name col.
+	 */
 	@FXML
 	private TableColumn<Part, String> associatedPartNameCol;
+	/**
+	 * The Associated part inv lvl col.
+	 */
 	@FXML
 	private TableColumn<Part, Integer> associatedPartInvLvlCol;
+	/**
+	 * The Associated part cost col.
+	 */
 	@FXML
 	private TableColumn<Part, Double> associatedPartCostCol;
+	/**
+	 * The Product id text field.
+	 */
 	@FXML
 	private TextField productIdTextField;
+	/**
+	 * The Product name text field.
+	 */
 	@FXML
 	private TextField productNameTextField;
+	/**
+	 * The Product inv text field.
+	 */
 	@FXML
 	private TextField productInvTextField;
+	/**
+	 * The Product price text field.
+	 */
 	@FXML
 	private TextField productPriceTextField;
+	/**
+	 * The Product max text field.
+	 */
 	@FXML
 	private TextField productMaxTextField;
+	/**
+	 * The Product min text field.
+	 */
 	@FXML
 	private TextField productMinTextField;
 
+	/**
+	 * Searches for product by name or ID.
+	 * <p>
+	 * If no product is found, an error message will be displayed,
+	 * the search field will clear, and the table will be refreshed.
+	 *
+	 * @param actionEvent defines the action of the search field.
+	 */
 	public void partSearchOnKeyPress(ActionEvent actionEvent) {
 		TextField search = (TextField) actionEvent.getSource();
 		String input = search.getText();
@@ -71,6 +129,11 @@ public class AddProductController implements Initializable {
 		CommonFunctions.getSearchResults(search, input, allParts, partsTableView);
 	}
 
+	/**
+	 * Add to associated parts table and list on click.
+	 *
+	 * @param actionEvent the action event.
+	 */
 	public void addToAssociatedPartsOnClick(ActionEvent actionEvent) {
 		Part selectedPart = partsTableView.getSelectionModel().getSelectedItem();
 		try {
@@ -82,6 +145,11 @@ public class AddProductController implements Initializable {
 		}
 	}
 
+	/**
+	 * Remove associated part on click.
+	 *
+	 * @param actionEvent the action event.
+	 */
 	public void removeAssociatedPartOnClick(ActionEvent actionEvent) {
 		Part selectedPart = associatedPartsTableView.getSelectionModel().getSelectedItem();
 		try {
@@ -93,6 +161,15 @@ public class AddProductController implements Initializable {
 		}
 	}
 
+	/**
+	 * Save product if validation checks are passed.
+	 * Included appropriate error messages. Includes
+	 * logger to log errors just to get some practice
+	 * with them.
+	 *
+	 * @param actionEvent the action event.
+	 * @throws IOException the io exception.
+	 */
 	@FXML
 	void saveProductOnClick(ActionEvent actionEvent) throws IOException {
 		int id = Inventory.getAllProducts().size() + 1;
@@ -105,7 +182,6 @@ public class AddProductController implements Initializable {
 			int productMax = Integer.parseInt(productMaxTextField.getText());
 			int productMin = Integer.parseInt(productMinTextField.getText());
 
-
 			if (!(ErrMsg.verifyMin(productMin, productMax)) || !(ErrMsg.verifyInv(productInv, productMin, productMax))) {
 				System.Logger logger = System.getLogger("AddProductController");
 				logger.log(System.Logger.Level.ERROR, "Invalid input");
@@ -116,8 +192,6 @@ public class AddProductController implements Initializable {
 				}
 				Inventory.addProduct(product);
 				ReturnToMainWindow.mainWindow(actionEvent);
-				System.out.println("product added");
-
 			}
 		} catch (NumberFormatException e) {
 			System.Logger logger = System.getLogger("AddProductController");
@@ -125,16 +199,22 @@ public class AddProductController implements Initializable {
 		}
 	}
 
+	/**
+	 * Cancel product on click.
+	 *
+	 * @param actionEvent the action event.
+	 * @throws IOException the io exception.
+	 */
 	@FXML
 	void cancelProductOnClick(ActionEvent actionEvent) throws IOException {
 		ReturnToMainWindow.mainWindow(actionEvent);
 	}
 
 	/**
-	 * Initialize.
+	 * Initializes the Parts and Associated Parts table views.
 	 *
-	 * @param location  the location
-	 * @param resources the resources
+	 * @param location  the location.
+	 * @param resources the resources.
 	 */
 	@Override
 	public void initialize(URL location, ResourceBundle resources) {
@@ -142,7 +222,4 @@ public class AddProductController implements Initializable {
 				partCostCol, associatedPartsTableView, associatedParts, associatedPartIdCol,
 				associatedPartNameCol, associatedPartInvLvlCol, associatedPartCostCol);
 	}
-
-
 }
-
